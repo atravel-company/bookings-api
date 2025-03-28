d12de4236853d31161254cbdd2821288902bfc0d
Patrick M <patrickmatias.silva@hotmail.com>
2025-03-28 15:33:30 +0000

    feat: log who created user on user creation

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -10,11 +10,15 @@ class UserObserver
     public function created(User $user)
     {
         if (Str::endsWith($user->email, ['@gmail.com', '@yahoo.com'])) {
+            $creator = auth()->user();
+
             \Log::channel('monitored_users')->info('User created using a suspicious email address', [
                 'user_id'    => $user->id,
                 'email'      => $user->email,
                 'ip_address' => request()->ip(),
                 'user_agent' => request()->userAgent(),
+                'creator_id'   => $creator ? $creator->id : null,
+                'creator_email'=> $creator ? $creator->email : null,
             ]);
         }
     }
