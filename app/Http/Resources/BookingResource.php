48bae3b380b0f1ca1095bbc54a7243e4b9256e0a
Patrick M <patrickmatias.silva@hotmail.com>
2025-05-08 15:34:54 +0000

    chore: rename `ReportResource` => `BookingResource`

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ReportResource extends JsonResource
+class BookingResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
